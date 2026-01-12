0b2e2c2a4c9425f954cb01b84fa53df2bd3a0f53
gabriel <blanckaertgabriel@Gabriel>
2026-01-12 16:07:43 +0100

    initialization

---
+++ classes/Joueur.js
@@ -0,0 +1,17 @@
+class Joueur {
+	// constructor(name, level, speed, health, couldown, damage, isDead, isAttacking, isMoving, position){
+	// 	this.name = name;
+	// 	this.level = level;
+	// 	this.speed = speed;
+	// 	this.health = health;
+	// 	this.couldown = couldown;
+	// 	this.damage = damage;
+	// 	this.isDead = isDead;
+	// 	this.isAttacking = isAttacking;
+	// 	this.isMoving = isMoving;
+	// 	this.position = position;
+	// }
+	constructor(name){
+		this.name = name;
+	}
+}
